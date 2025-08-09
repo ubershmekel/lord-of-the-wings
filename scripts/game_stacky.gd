a8875e49fe26b030d834417143f620845a0680e6
Yuval Greenfield <ubershmekel@gmail.com>
2025-08-08 23:28:25 -0700

    Physics was a mistake It's all madness I'm going to erase it all and start over

--- scripts/game_stacky.gd
+++ scripts/game_stacky.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const GRAVITY = 1000
+const FORWARD_SPEED = 150
 var scroll_speed: float = 150.0
 var scroll_offset: float = 0.0
 @onready var screen_size = get_viewport_rect().size
@@ -11,11 +12,19 @@ var scroll_offset: float = 0.0
 @onready var camera = $Camera2D
 
 var block_scene = preload("res://scenes/stacky_block.tscn")
-const FORWARD_SPEED = 150
+var level_chunk_scene = preload("res://scenes/stacky_level_chunk.tscn")
+
+@onready var debug_label: Label = $UI/DebugLabel
 
 func _ready():
 	bird_body.position.y = bird_base_y
 	bird_body.position.x = 0
+	
+	for i in range(100):
+		var chunk = level_chunk_scene.instantiate()
+		chunk.position.x = i * block_height
+		chunk.position.y = 300
+		$Level.add_child(chunk)
 
 func _process(delta):
 	scroll_offset += scroll_speed * delta
@@ -26,30 +35,72 @@ func _process(delta):
 
 func get_block_height():
 	var block = block_scene.instantiate()
-	var sprite = block.get_node("Sprite2D")  # Adjust path as needed
-	var block_height = sprite.texture.get_height() * sprite.scale.y
+	var block_body = block.get_node("BlockBody")
+	var sprite = block_body.get_node("Sprite2D")  # Adjust path as needed
+	var height = sprite.texture.get_height() * sprite.scale.y
 	block.queue_free()  # Don’t keep it in the scene
-	return block_height
+	return height
 
 func add_block():
 	var block = block_scene.instantiate()
-	block.position = Vector2(0, block_height * ($BlockStack.get_child_count() + 1))
+	#block.position = Vector2(0, block_height * ($BlockStack.get_child_count() + 1))
+	var block_body = block.get_node("BlockBody")
+	#block_body.global_position.y = bird_body.global_position.y
+	#block_body.position.y = bird_body.position.y + block_height
+	#block_body.position.x = bird_body.position.x
+	#block_body.position.x = 0
+	#block.position.x = bird_body.position.x
+	block.global_position.y = bird_body.global_position.y
+
 	$BlockStack.add_child(block)
-	print(block)
 
 func _physics_process(delta):
 	bird_body.velocity.x = FORWARD_SPEED
 	bird_body.velocity.y += GRAVITY * delta
-	$BlockStack.position = $Bird.position
+	#$BlockStack.position = $Bird.position
+	if $BlockStack.get_child_count() > 0:
+		var first_block = $BlockStack.get_child(0)
+		var block_body = first_block.get_node("BlockBody")
+		#debug_label.text = "%f %f" % [first_block.position.x, first_block.position.y]
+		#debug_label.text = "%s\n%s\n%s\n%s" % [$BlockStack.position, first_block.position, block_body.position, bird_body.position]
+		#var pos = [bl for bl in $BlockStack.get_children()]
+		#debug_label.text = "\n".join([
+			
+			#$BlockStack.global_position ,
+			#block_body.global_position,
+			#first_block.global_position,
+			#first_block.get_child(0).global_position,
+			#bird_body.position
+		#])
+		
+		var positions = [bird_body.global_position]
+		for block in $BlockStack.get_children():
+			var bbody = block.get_node("BlockBody")
+			positions.append(bbody.position)
+		debug_label.text = "\n".join(positions)
+
+
 	if Input.is_action_just_pressed("ui_accept"):
-		add_block()
-		print(bird_base_y, " ", bird_body.position.y, " ", bird_body.velocity.y)
-		bird_body.position.y -= block_height
+		#print(bird_body.position.x)
+		#print(bird_base_y, " ", bird_body.position.y, " ", bird_body.velocity.y)
+		bird_body.global_position.y -= block_height * 3
 		bird_body.velocity.y = 0
 		bird_sprite.sprite_frames.set_animation_loop("flap", false)
 		bird_sprite.play("flap")
+		add_block()
 		
 	
+	for block in $BlockStack.get_children():
+		var block_body = block.get_node("BlockBody")
+		block_body.global_position.x = bird_body.global_position.x
+		#block.position.x = block_height
+		#block_body.velocity.x = FORWARD_SPEED
+		block_body.velocity.y += GRAVITY * delta
+		block_body.move_and_slide()
+
+	#$BlockStack.position.x = bird_body.position.x
+	#print("block.position: ", $BlockStack.position)
+	#print("bird_body.position: ", bird_body.position)
 	bird_body.move_and_slide()
 
 func spawn_block():
